2a3e5f93c3552d35c1e7d8fdf3658d3d59b22e92
QimbE <58182585+QimbE@users.noreply.github.com>
2023-09-19 12:25:09 +0500

    feat: details page add

--- WebApplication1/Areas/Customer/Controllers/HomeController.cs
+++ WebApplication1/Areas/Customer/Controllers/HomeController.cs
@@ -22,8 +22,13 @@ namespace TestShopProject.Areas.Customer.Controllers
 	        IEnumerable<Product> productList = _unitOfWork.Product.GetAll("Category");
             return View(productList);
         }
+        public IActionResult Details(int id)
+        {
+			Product product = _unitOfWork.Product.Get(x=> x.Id == id,"Category");
+	        return View(product);
+        }
 
-        public IActionResult Privacy()
+		public IActionResult Privacy()
         {
             return View();
         }

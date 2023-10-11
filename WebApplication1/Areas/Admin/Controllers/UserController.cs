85c9f1dbfb3a6654ba1152e7c5effeea2672d5d3
QimbE <58182585+QimbE@users.noreply.github.com>
2023-10-11 13:42:17 +0500

    feat: user role managment add

--- WebApplication1/Areas/Admin/Controllers/UserController.cs
+++ WebApplication1/Areas/Admin/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TestShop.DataAccess.Data;
 using TestShop.DataAccess.Repository.IRepository;
@@ -16,16 +17,89 @@ namespace TestShopProject.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public UserController(ApplicationDbContext db)
+		private readonly UserManager<IdentityUser> _userManager;
+        public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
         {
 	        _db = db;
-
+			_userManager = userManager;
 		}
         public async Task<IActionResult> Index()
         {
             return View();
         }
-        
+
+        public async Task<IActionResult> RoleManagment(string userId)
+        {
+	        string roleId = (await _db.UserRoles.FirstOrDefaultAsync(x => x.UserId == userId)).RoleId;
+	        RoleManagmentVM roleVM = new()
+	        {
+                ApplicationUser = await _db.ApplicationUsers
+	                .Include(x=> x.Company)
+	                .FirstOrDefaultAsync(x => x.Id == userId),
+                RoleList = _db.Roles.Select(x => new SelectListItem()
+                {
+                    Text = x.Name,
+                    Value = x.Name
+                }),
+                CompanyList = _db.Companies.Select(x => new SelectListItem()
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+				})
+	        };
+
+	        roleVM.ApplicationUser.Role = (await _db.Roles.FirstOrDefaultAsync(x => x.Id == roleId)).Name;
+
+	        return View(roleVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RoleManagment(RoleManagmentVM roleManagmentVM)
+        {
+	        string roleId = (await _db.UserRoles.FirstOrDefaultAsync(x => x.UserId == roleManagmentVM.ApplicationUser.Id)).RoleId;
+	        string oldRole = (await _db.Roles.FirstOrDefaultAsync(x => x.Id == roleId)).Name;
+
+	        if (roleManagmentVM.ApplicationUser.Role != oldRole)
+	        {
+				//user role was updated
+				ApplicationUser applicationUser = await _db.ApplicationUsers
+					.Include(x => x.Company)
+					.FirstOrDefaultAsync(x => x.Id == roleManagmentVM.ApplicationUser.Id);
+
+				if (roleManagmentVM.ApplicationUser.Role == StaticDetails.Role_Company)
+				{
+					//assign to company
+					applicationUser.CompanyId = roleManagmentVM.ApplicationUser.CompanyId;
+				}
+
+				if (oldRole == StaticDetails.Role_Company)
+				{
+					//unassign user
+					applicationUser.CompanyId = null;
+				}
+
+				await _db.SaveChangesAsync();
+
+				await _userManager.RemoveFromRoleAsync(applicationUser, oldRole);
+
+				await _userManager.AddToRoleAsync(applicationUser, roleManagmentVM.ApplicationUser.Role);
+	        }
+	        else if (roleManagmentVM.ApplicationUser.Role == StaticDetails.Role_Company)
+	        {
+				//if it is a company user, we should check if the company changed
+		        ApplicationUser applicationUser = await _db.ApplicationUsers
+			        .Include(x => x.Company)
+			        .FirstOrDefaultAsync(x => x.Id == roleManagmentVM.ApplicationUser.Id);
+				if (applicationUser.CompanyId != roleManagmentVM.ApplicationUser.CompanyId)
+		        {
+			        applicationUser.CompanyId = roleManagmentVM.ApplicationUser.CompanyId;
+			        await _db.SaveChangesAsync();
+				}
+
+	        }
+			//to the user list page
+	        return RedirectToAction(nameof(Index));
+        }
         #region API CALLS
 
         [HttpGet]
